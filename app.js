a9e5a9306741dd66af0f9e0658cd086f1701bf61
codehub55 <codehub555@email.com>
2023-05-11 06:09:20 +0530

    chilled file  add

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-const print = "this page is use for js";
-alert(print);
+
+    const print = "this page is use for js";
+    alert(print);
+
+    document.addEventListener("keypress", function(event) {
+
+        makeSound(event.key);
+      
+        buttonAnimation(event.key);
+      
+      });
